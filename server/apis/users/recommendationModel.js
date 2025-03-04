8db0bdd8a49f5e205f173caf1eb6213e203d4540
TzivyaFireman <tz0534199716@gmail.com>
2025-03-04 19:06:03 +0200

    Add recommendation model

---
+++ server/apis/users/recommendationModel.js
@@ -0,0 +1,15 @@
+// Tzivya Fireman
+// recommendation model
+const mongoose = require('mongoose');
+
+const RecommendationSchema = new mongoose.Schema({
+  testCode: { type: String, ref: 'Test', required: true },
+  personalityString: { type: String, required: true },
+  title: { type: String, required: true },
+  general: { type: String },
+  recommendations: [{ type: String }],
+});
+
+const Recommendation = mongoose.model('Recommendation', RecommendationSchema);
+
+module.exports = Recommendation;
