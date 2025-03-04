fa75c465ae30ee998d5998625c3250bee1c89cfa
Friedman Rivka <151746698+friedmanrivka@users.noreply.github.com>
2025-03-04 20:43:57 +0200

    Added MongoDB connection using Mongoose

--- server/index.js
+++ server/index.js
@@ -3,11 +3,14 @@ const app = express();
 const cors = require('cors');
 const fs = require('fs');
 const yaml = require('js-yaml');
+require('dotenv').config();
+const connectDB = require('./common/db')
 app.use(cors());
 
 const config = yaml.load(fs.readFileSync('./config/config.yaml', 'utf8'));
 const port = config.server.port;
 
+connectDB();
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
