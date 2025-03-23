ea9e2d0ef2e4cc49c180229d47886c5550f28ef1
Tzipi-Winefeld <157100435+Tzipi-Winefeld@users.noreply.github.com>
2025-03-23 02:28:57 +0200

    fix style header

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import styles from "./Header.module.scss";
 import AvatarIcon from "../AvatarIcon/AvatarIcon";
-import logo from "../../assets/logo_book.png"; // Assuming you have a logo file
+import logo from "../../assets/logo_book.png";
 
 
 const Header = () => {
@@ -21,34 +21,38 @@ const Header = () => {
     return (
         <nav className={styles.navbar}>
             <div className={styles.navContainer}>
-                <div className={styles.logo}>
-                    <NavLink to="/">
-                        <img src={logo} alt="Test" />
-                        <span>מבחן התאמה תורני</span>
-                    </NavLink>
+                <div className={styles.navGroup}>
+                    <div className={styles.logo}>
+                        <NavLink to="/">
+                            <img src={logo} alt="Test" />
+                            <span>מבחן התאמה תורני</span>
+                        </NavLink>
+                    </div>
+                    <ul className={styles.navList}>
+                        <li>
+                            <NavLink to="/" className={({ isActive }) => (isActive ? styles.active : "")}>בית</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/about" className={({ isActive }) => (isActive ? styles.active : "")}>אודות</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/contact" className={({ isActive }) => (isActive ? styles.active : "")}>יצירת קשר</NavLink>
+                        </li>
+                    </ul>
                 </div>
-                <ul className={styles.navList}>
-                    <li>
-                        <NavLink to="/" className={({ isActive }) => (isActive ? styles.active : "")}>בית</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/about" className={({ isActive }) => (isActive ? styles.active : "")}>אודות</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/contact" className={({ isActive }) => (isActive ? styles.active : "")}>יצירת קשר</NavLink>
-                    </li>
-                </ul>
-                {isLoggedIn ? (
-                    <NavLink to="/profile">
-                        <AvatarIcon firstName={user.firstName} lastName={user.lastName} size={40} />
-                    </NavLink>
-                ) : (
-                    <NavLink to="/login">
-                        {/* generic button */}
-                        <button>התחברות / הרשמה</button>
-                    </NavLink>
-                )}
-
+                <div className={styles.profile}>
+                    {isLoggedIn ? (
+                        <NavLink to="/profile" className={styles.profileLink}>
+                            <span className={styles.profileText}>אזור אישי</span>
+                            <AvatarIcon firstName={user.firstName} lastName={user.lastName} size={40} />
+                        </NavLink>
+                    ) : (
+                        <NavLink to="/login">
+                            {/* generic button */}
+                            <button>התחברות / הרשמה</button>
+                        </NavLink>
+                    )}
+                </div>
             </div>
         </nav>
     );
