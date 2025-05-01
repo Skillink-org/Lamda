f42ac0b1256c9c05aeb8806c101775ab9c3fd392
diti bash <diti0527665315@gmail.com>
2025-05-01 15:57:23 +0300

    Adding logs

--- server/apis/exams-tests/testRepo.js
+++ server/apis/exams-tests/testRepo.js
@@ -2,30 +2,48 @@
 
 const Test = require("../../models/testModel");
 const Category = require("../../models/categoryModel");
-const Quetion = require("../../models/questionModel");
+const Question = require("../../models/questionModel");
 
-const getTestByName = async(name= 'MBTI')=>{
+const getTestByName = async (name = "MBTI להתאמת סגנון לימוד תורני") => {
+  try {
+    // שליפת המבחן לפי השם
+    const test = await Test.findOne({ name });
 
-    try {
-        // שליפת המבחן לפי השם
-        const test = await Test.findOne({ name });
-    
-        if (!test) {
-          throw new Error('Test not found');
-        }
-    
-        // שליפת הקטגוריות עבור המבחן
-        const categories = await Category.find({ testId: test._id }).populate('questions');
-    
-        // החזרת המבחן עם הקטגוריות והשאלות
+    if (!test) {
+      throw new Error("Test not found");
+    }
+
+    // שליפת הקטגוריות עבור המבחן
+    const categories = await Category.find({ testId: test._id });
+
+    // שליפת השאלות עבור כל קטגוריה
+    const categoriesWithQuestions = await Promise.all(
+      categories.map(async (category) => {
+        const questions = await Question.find({ categoryId: category._id });
         return {
-          ...test.toObject(), // המרת המבחן לאובייקט רגיל
-          categories, // הוספת הקטגוריות
+          ...category.toObject(),
+          questions, // הוספת השאלות לקטגוריה
         };
-      } catch (error) {
-        console.error("Error fetching test:", error);
-        throw error; // ניתן לנהל את השגיאה לפי הצורך
-      }
+      })
+    );
 
-}
-module.exports ={ getTestByName}
+    // לוג של הקטגוריות והשאלות
+    console.log("Categories found:", categoriesWithQuestions);
+    categoriesWithQuestions.forEach((category) => {
+      console.log(
+        `Category: ${category.name}, Questions:${category.questions}`
+      );
+    });
+
+    // החזרת המבחן עם הקטגוריות והשאלות
+    return {
+      ...test.toObject(), // המרת המבחן לאובייקט רגיל
+      categories: categoriesWithQuestions, // הוספת הקטגוריות עם השאלות
+    };
+  } catch (error) {
+    console.error("Error fetching test:", error);
+    throw error; // ניתן לנהל את השגיאה לפי הצורך
+  }
+};
+
+module.exports = { getTestByName };
